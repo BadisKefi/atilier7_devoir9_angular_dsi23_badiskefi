ad3273dada5d05c09b36bc79cd43d92efb8140dc
BadisKefi <86252062+BadisKefi@users.noreply.github.com>
2022-12-02 10:10:26 +0100

    Add files via upload

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+import { ActivatedRoute,Router } from '@angular/router';
+import { Categorie } from '../model/categorie.model';
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+  styleUrls: ['./add-produit.component.css']
+})
+export class AddProduitComponent implements OnInit {
+  newProduit = new Produit();
+  categories! : Categorie[];
+  newIdCat! : number;
+  newCategorie! : Categorie;
+
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private router :Router,
+    private produitService: ProduitService) { }  
+    ngOnInit() {
+      this.categories = this.produitService.listeCategories();
+    }
+    addProduit() {
+      this.newCategorie = this.produitService.consulterCategorie(this.newIdCat);
+      this.newProduit.categorie = this.newCategorie;
+      this.produitService.ajouterProduit(this.newProduit)
+      /*.subscribe(prod => { console.log(prod); })*/;
+      this.router.navigate(['produits']);
+      }
+
+
+}
